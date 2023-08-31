660db10fd7df742891e1f3745576b437f3712fae
tferguson <tferguson@gaig.com>
2023-08-31 14:05:09 -0400

    still working on initial commit

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        image: './images/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: true
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      inStock: false,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: './images/vmSocks-green-onWhite.jpg'
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: './images/vmSocks-blue-onWhite.jpg'
+        }
+      ],
+      cart: 0
+    },
+    selectedVariant: 0,
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(index) {
+        this.selectedVariant = index
+        console.log(index)
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
+    },
+    computed: {
+      title () {
+        return this.brand + ' ' + this.product
+      },
+      image () {
+        return this.variants[this.selectedVariant].variantImage
+      }
     }
-})
+  })
